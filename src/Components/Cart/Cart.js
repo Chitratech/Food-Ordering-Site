951528083795cec30b353e414f2a9c1e833802c1
Chitra <chitraraj94@gmail.com>
2021-08-26 23:14:54 +0530

    cart changes

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,40 +1,30 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
-
-const Cart=()=>{
-
-
-    const cartItem=(<ul className={classes['cart-items']}> {[ {
-id:'c1' , name:'briyani' , amount:2 , price: 120
-
-    }].map( (item)=>( <li>{item.name}</li>) )
-    }
-
-
-
-    </ul>)
-
-
-    }
-
-    return(
-        <div> 
-<div> className={classes.total}
-    {cartItem}
-    <span> Total Amount:</span>
-<span>33.4 </span>
-</div>
-
-<div className={classes.actions}>
-<button className={classes['.button--alt']}> close</button>
-<button className={classes.button}>order</button>
-</div>
-
-</div>
-
-    );
-
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
 };
 
 export default Cart;
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,26 +1,21 @@
-import React, { Fragment } from 'react';
-import classes from './Header.module.css';
-import mealsImage from '../../assets/meals.png'
+import { Fragment } from 'react';
+
 import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.png';
+import classes from './Header.module.css';
 
 const Header = (props) => {
-
-
-    return (
+  return (
     <Fragment>
-        <header className={classes.header}>
-            <h1>RC Meals </h1>
-           < HeaderCartButton />
-        </header>
-
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt="table full of food" />
-        </div>
-
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
     </Fragment>
-
-
-    );
+  );
 };
 
 export default Header;
